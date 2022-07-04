4d60ab3ba8a7c0e2d24f6dd45db8391d075b0c46
julianjauk <jaujuc18@htl-kaindorf.at>
2022-07-04 17:09:24 +0200

    BUG: no solution for opening the Website with cook

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,7 @@
 import clear from 'clear';
 import figlet from 'figlet';
 import chalk from 'chalk';
+import open from 'open';
 const CLI = require('clui');
 const Spinner = CLI.Spinner;
 const inquirer = require('./lib/inquirer.lib.ts');
@@ -32,10 +33,34 @@ module.exports = {
       conf.set(credentials);
       status.stop();
       console.log(chalk.green('Logged in ✅'));
-      await inquirer.askPDF();
+      let answer = await inquirer.askPDF();
+      if (answer) {
+        // get the date via inquirer
+        downloadPDF(client);
+      } else {
+        // get mails and send to BAUER
+        sendMail(client);
+      }
     } catch (e) {
       console.log('\n' + chalk.red('Invalid Credentials please try again!'));
     }
     status.stop();
   },
 };
+
+const downloadPDF = async (client) => {
+  // fixme: command prompt won't open instantly
+  console.log(
+    chalk.blue('Please enter the beginning of your Schoolyear (Press =>): ')
+  );
+  let value = await inquirer.askDate();
+  let date = new Date(value.date);
+  let URL = await client.getPdfOfAbsentLesson(date, new Date(), true);
+  console.log(
+    chalk.blue(
+      'Please copy and paste the following link into your Browser and login into Webuntis, if you are not redirected to the page you should go to the link again! Sorry for this bug, but this Feature is still in progress :/ \n'
+    ) + URL
+  );
+};
+
+const sendMail = (client) => {};
